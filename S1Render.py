742de29ad46fe2d0ab9d9c1c0ac96c4f1cb3e7b4
xdsoar <xdsoar@gmail.com>
2019-01-12 13:30:54 +0800

    WIP finish template

--- S1Render.py
+++ S1Render.py
@@ -18,7 +18,15 @@ settings.configure(TEMPLATES=TEMPLATES)
 django.setup()
 
 
-def renderHTML(text):
-    t = Template(open('s1_template.html').read())
-    c = text
-    return str(t.render(c))
+def renderHTML(template, text):
+    t = Template(open('template/'+template).read())
+    return t.render(text)
+
+if __name__ == "__main__":
+    postContext = Context({'post_author': 'hello', 'post_content': 'here is the content'})
+    a = renderHTML('s1_post_template.html', postContext)
+    bodyContext = Context({'upname': 'saraba1st', 'title': 'test s1 post', 'postlist': a})
+    body = renderHTML('s1_post_body_template.html', bodyContext)
+    html = renderHTML('s1_base_template.html', Context({'body': body}))
+    output = open('output.html', 'w')
+    output.write(html)
